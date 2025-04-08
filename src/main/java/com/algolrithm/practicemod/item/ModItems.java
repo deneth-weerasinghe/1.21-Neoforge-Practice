e837b782c6935381bbfa5614057d893ca2feaf99
Deneth Weerasinghe <deneth@hotmail.co.uk>
2025-04-09 00:12:03 +0100

    Added durability to chiseling wand item

--- src/main/java/com/algolrithm/practicemod/item/ModItems.java
+++ src/main/java/com/algolrithm/practicemod/item/ModItems.java
@@ -1,10 +1,15 @@
 package com.algolrithm.practicemod.item;
 
 import com.algolrithm.practicemod.PracticeMod;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.component.Tool;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.List;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(PracticeMod.MOD_ID);
@@ -19,7 +24,13 @@ public class ModItems {
             () -> new Item(new Item.Properties())
     );
     public static final DeferredItem<Item> CHISEL_WAND = ITEMS.register("chisel_wand",
-            () -> new ChiselItem(new Item.Properties().durability(2048))
+            () -> new ChiselItem(
+                    new Item.Properties()
+                            .rarity(Rarity.RARE)
+                            .durability(2048)
+                            .component(DataComponents.TOOL,
+                                    new Tool(List.of(), 1.0F, 2))
+            )
     );
 
     public static void register(IEventBus eventBus) {
